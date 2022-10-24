714659c56189dbbdf6ec74c9f82a41bc7387c5fe
ebGinblat <elias.ginblat@stu.ocsb.ca>
2022-10-24 14:37:52 -0400

    Update FixMe.java

--- CCC2019/src/ccc2019/FixMe.java
+++ CCC2019/src/ccc2019/FixMe.java
@@ -1,15 +1,17 @@
 //This should be able to solve J3 as seen in Instructions.pdf.
 package ccc2019;
 
+import java.util.Scanner;
+
 /**
  *
  * @author SPH_SHSM
  */
 public class FixMe {
 public static void main(String[] args) {
-scanner in = new Scanner(System.in);
+Scanner in = new Scanner(System.in);
 //Read in a number
-  int lines = in.nextInt;
+  int lines = in.nextInt();
      in.nextLine();
      									string output="";
         //Loop through the lines
@@ -18,17 +20,17 @@ scanner in = new Scanner(System.in);
  String input = in.nextline();
             //Loop through the characters
  
- char currentChar="a"
- Integer currentCount='0';
+ char currentChar="a";
+ Int currentCount='0';
  Char previousChar;
  For (int j = 0; j < input.length j++) 
        //if this is the first pass through the loop
  			if (j=0){
                     //current char is the char at j
  										currentChar = input.charAt(j);
-                    //current count = 1
-      currentCount=2;
- 											}
+//current count = 1
+currentCount=2;
+}
                 //else
                 else
                     //set previousChar to currentChar
